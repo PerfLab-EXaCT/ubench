4a76763cce4f2a324561cb6b0f2fbfb3e8d82996
Tallent, Nathan R <nathan.tallent@pnnl.gov>
2018-07-16 23:00:14 +0000

    update.
    git-svn-id: https://svn.pnl.gov/svn/hpcgroup/projects/modsim-tools/ubench@17722 4c623a4d-1e8e-0410-8a3e-ee3132d44425

---
+++ perf-intel-pebs-attribution/y-4nop.s
@@ -0,0 +1,130 @@
+	.file	"y.c"
+	.globl	SIZE
+	.data
+	.align 8
+	.type	SIZE, @object
+	.size	SIZE, 8
+SIZE:
+	.quad	10000000
+	.text
+	.globl	f1
+	.type	f1, @function
+f1:
+.LFB2:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movq	%rsi, -32(%rbp)
+	movl	$1, -4(%rbp)
+	movq	$0, -16(%rbp)
+	jmp	.L2
+.L3:
+	negl	-4(%rbp)
+	movq	-16(%rbp), %rax
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-4(%rbp), %eax
+	movl	%eax, (%rdx)
+	addq	$1, -16(%rbp)
+.L2:
+	movq	-16(%rbp), %rax
+	cmpq	-32(%rbp), %rax
+	jb	.L3
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	f1, .-f1
+	.globl	f2
+	.type	f2, @function
+f2:
+.LFB3:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movq	%rsi, -32(%rbp)
+	movq	%rdx, -40(%rbp)
+	movq	$1, -8(%rbp)
+	jmp	.L5
+.L6:
+	movq	-8(%rbp), %rax
+	leaq	0(,%rax,4), %rdx
+	movq	-32(%rbp), %rax
+	addq	%rdx, %rax
+	movq	-8(%rbp), %rdx
+	leaq	0(,%rdx,4), %rcx
+	movq	-24(%rbp), %rdx
+	addq	%rcx, %rdx
+	movl	(%rax), %eax
+	movl	%eax, (%rdx)
+	addq	$1, -8(%rbp)
+.L5:
+	movq	-40(%rbp), %rax
+	subq	$1, %rax
+	cmpq	%rax, -8(%rbp)
+	jb	.L6
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE3:
+	.size	f2, .-f2
+	.globl	main
+	.type	main, @function
+main:
+.LFB4:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	SIZE(%rip), %rax
+	salq	$2, %rax
+	movq	%rax, %rdi
+	call	malloc
+	movq	%rax, -8(%rbp)
+	movq	SIZE(%rip), %rax
+	salq	$2, %rax
+	movq	%rax, %rdi
+	call	malloc
+	movq	%rax, -16(%rbp)
+	movq	SIZE(%rip), %rdx
+	movq	-8(%rbp), %rax
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	f1
+	movq	SIZE(%rip), %rdx
+	movq	-16(%rbp), %rcx
+	movq	-8(%rbp), %rax
+	movq	%rcx, %rsi
+	movq	%rax, %rdi
+	call	f2
+	movq	-8(%rbp), %rax
+	movq	%rax, %rdi
+	call	free
+	movq	-16(%rbp), %rax
+	movq	%rax, %rdi
+	call	free
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE4:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 7.1.0"
+	.section	.note.GNU-stack,"",@progbits
